3df86022fa7314da47cb252f0a9f5efeb17a9bd5
Keith Leonardo <kleonardo52@gmail.com>
2021-05-07 18:50:22 +1000

    Remove homepage data

--- customer/src/homePage.js
+++ customer/src/homePage.js
@@ -77,42 +77,4 @@ function HomePage(props) {
   );
 }
 
-const homePageData = {
-    homepage: "",
-    rectangle9: "",
-    ellipse1: "",
-    address: "242 St Kilda Rd, St Kilda VIC 3182",
-    address2: "20 km",
-    address3: "6 km",
-    address4: "9 km",
-    fancyTruck3: "Fancy Truck",
-    address5: "242 St Kilda Rd, St Kilda VIC 3182",
-    fancyTruck22: "Fancy Truck",
-    address6: "242 St Kilda Rd, St Kilda VIC 3182",
-    jamesTruck: "Jame’s  Truck",
-    searchALocation: "Search a location",
-    untitledDesign20210303T1912562: "",
-    untitledDesign20210303T1912568: "",
-    untitledDesign20210303T1912569: "",
-    untitledDesign20210303T19125612: "",
-    untitledDesign20210303T19125610: "",
-    untitledDesign20210303T1912563: "",
-    untitledDesign20210303T1912564: "",
-    address7: "242 St Kilda Rd, St Kilda VIC 3182",
-    address8: "21 km",
-    fancyTruck32: "Fancy Truck",
-    untitledDesign20210303T19125611: "",
-    untitledDesign20210310T1032552: "",
-    untitledDesign20210310T1735541: "",
-    untitledDesign20210310T1811463: "",
-    spanText: "Available Trucks in ",
-    spanText2: "Melbourne",
-    untitledDesign20210303T1912561: "",
-    coolTruck: "Cool Truck",
-    address9: "1 km",
-    address10: "250 Lower Heidelberg Rd, Fairfield VIC 3079",
-    address11: "250 Lower Heidelberg Rd, Fairfield VIC 3079",
-};
-
-// button example
 export default HomePage;
