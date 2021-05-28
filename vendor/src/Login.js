0439dcd03a2cb60815e478d3bf2337298802b58f
Mitchell Needham <60385876+MitchellNeedham@users.noreply.github.com>
2021-05-28 19:38:14 +1000

    Login Page update

---
+++ vendor/src/Login.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { Link, Redirect } from "react-router-dom";
+import axios from "axios";
+import "./Login.css";
+
+function LoginPage(props) {
+  const [vendor_id, setVendorID] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirectHome, setRedirectHome] = useState(false);
+
+  function validateForm() {
+    return vendor_id.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let postData = { vendor_id, password };
+    console.log(postData);
+    axios
+      .post(
+        `https://info30005-customer-backend.herokuapp.com/api/vendor/login`,
+        postData
+      )
+      .then((res) => {
+        // Set global auth token for whenever an axios request is sent
+        localStorage.setItem('token', res.data.token);
+        setRedirectHome(true);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  if (redirectHome) {
+    return <Redirect to="/" />;
+  }
+  return (
+
+    <div className="Login">
+      <div className="left">
+      <img className="logo-image" src="https://i.imgur.com/kiMFyeA.png" />
+      <div className="header">
+        <h2 className="animation a1">Welcome!</h2>
+        <h4 className="animation a2"> Log in to your account with your vendor ID and password.</h4>
+      </div>
+      <Form className="form" onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="vendor_id">
+          <Form.Label className="form-name" ></Form.Label>
+          <Form.Control className="form-field animation a3" placeholder="Vendor ID"
+            autoFocus
+            type="Vendor ID"
+            value={vendor_id}
+            onChange={(e) => setVendorID(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label className="form-name" ></Form.Label>
+          <Form.Control className="form-field animation a4" placeholder="Password"
+            type="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <p className="animation a5"><a href="#">Forgot Password</a></p>
+        <Button
+          className="button"
+          block
+          size="lg"
+          type="submit"
+          disabled={!validateForm()}
+        >
+          Login
+        </Button>
+      </Form>
+      </div>
+      <div className="right"></div>
+    </div>
+
+  );
+}
+
+export default LoginPage;
--- vendor/src/index.js
+++ vendor/src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import HomePage from './HomePage';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
+import axios from 'axios';
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+import HomePage from "./HomePage";
+import LoginPage from "./Login";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HomePage />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
+// If user is logged in add token to request header
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if(token) {
+      config.headers.common.Authorization = `Bearer ${token}`;  
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
 
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={LoginPage} />
+        <Route path="/account" component={HomePage} />
+      </Switch>
+    </Router>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
