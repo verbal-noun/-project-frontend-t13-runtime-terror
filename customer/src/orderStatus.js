9b3a9b253a1ac83dc6ba220ad4428c083d704ae5
Kamyar <kkarimifakhr@student.unimelb.edu.au>
2021-05-29 20:39:42 +1000

    added timer to orderStatus page

--- customer/src/orderStatus.js
+++ customer/src/orderStatus.js
@@ -5,9 +5,23 @@ import "./orderStatus.css";
 import superManCoffee from "./assets/superManCoffee.png";
 
 
-function OrderStatus(props) {
-  let [redirect, setRedirect] = useState(null);
 
+
+
+
+
+function OrderStatus(props) {
+  
+  
+  
+  let [redirect, setRedirect] = useState(null);
+  let time = new Date().toLocaleString();
+  let d = time.split(' ')[1];
+  let current = d.substring(0,5)
+  let min = parseInt(d.substring(3,5)) + 10;
+  let modTime = current.substring(0,3) + min;
+  let disTime = current.substring(0,3) + (min + 5);
+  
   if(redirect) {
     console.log(redirect);
     return <Redirect to={redirect} />;
@@ -21,7 +35,8 @@ function OrderStatus(props) {
         alt="green tick illustration"
       />
       <h1 className="confirm-text">Thanks For Your Order!</h1>
-      <h2 className="remaining-time">Your order will be ready in xx:xx</h2>
+      <h2 className="remaining-time">You can modify your order before {modTime}</h2>
+      <h2 className="discount-time">20% Discount will be applied at {disTime}</h2>
       
       
       <div className="button-div">
