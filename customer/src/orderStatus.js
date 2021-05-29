6e0acbda209214c2147e9a211b3ed28baede2931
Keith Leonardo <kleonardo52@gmail.com>
2021-05-30 05:57:01 +1000

    Add cancel order functionality and fix timers in orderStatus page

--- customer/src/orderStatus.js
+++ customer/src/orderStatus.js
@@ -6,9 +6,28 @@ import superManCoffee from "./assets/superManCoffee.png";
 import axios from "axios";
 
 
+function elapsed(time) {
+  // order time calculation
+  time /= 1000;
+  var seconds = Math.round(time);
+  if(seconds >= 60 * 60) {
+    let hours = Math.round(seconds / (60 * 60));
+    if(hours == 1) return `${hours} hours`;
+    else return `${hours} hour`;
+  }
+  else if(seconds >= 60) {
+    let minutes = Math.round(seconds / 60); 
+    if(minutes == 1) return `${minutes} minute`;
+    else return `${minutes} minutes`;
+  }
+  else {
+    return `${seconds} seconds`; 
+  }
+}
 
 function OrderStatus(props) {
   let [order, setOrder] = useState({});
+  let [time, setTime] = useState(0);
   let [modifyTime, setModifyTime] = useState(0);
   let [discountTime, setDiscountTime] = useState(0);  
   let [redirect, setRedirect] = useState(null);
@@ -25,20 +44,37 @@ function OrderStatus(props) {
             for(let global of res.data) {
               globals[global.name] = global.value;
             }
-
+            
             // Calculate timestamps
             let startTime = new Date(order.modifiedAt).getTime();
             setModifyTime(startTime + globals.orderChangeLimit*60000);
             setDiscountTime(startTime + globals.discountLimit*60000);
         });
       });
+
+      let interval = setInterval(() => setTime(Date.now()), 1000);
+      return () => {
+        clearInterval(interval);
+      }
   }, []);
+
+  const cancelOrder = () => {
+    let data = props.location.state;
+    axios.post(`https://info30005-customer-backend.herokuapp.com/api/customer/cancelOrder/`, {orderID: data.selectedID})
+      .then(() => {
+        setRedirect("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+    });
+  }
 
   if(redirect) {
     console.log(redirect);
     return <Redirect to={redirect} />;
   }
-  // Visit a vendor page
+
+  // Edit order and cancel order buttons are disabled after respective times has elapsed
   return (
     <div className="confirm-card">
       <img
@@ -47,8 +83,8 @@ function OrderStatus(props) {
         alt="green tick illustration"
       />
       <h1 className="confirm-text">Thanks For Your Order!</h1>
-      <h2 className="remaining-time">You can modify your order before {new Date(Date.now() - modifyTime).toLocaleTimeString("en-us")}</h2>
-      <h2 className="discount-time">20% Discount will be applied at {new Date(Date.now() - discountTime).toLocaleTimeString("en-us")}</h2>
+      <h2 className="remaining-time">You have {elapsed(modifyTime - time)} left to modify your order</h2>
+      <h2 className="discount-time">{elapsed(discountTime - time)} until a 20% Discount is applied</h2>
       
       <div className="button-div">
         <Button className="button"
@@ -56,10 +92,18 @@ function OrderStatus(props) {
           title="Home"
           color="#047E61"
         >Home</Button>
+
         <Button className="button"
           onClick={() => setRedirect("/checkout")}
           color="#047E61"
-        >Order Page</Button>
+          disabled={() => modifyTime - time >= 0}
+        >Edit Order</Button>
+        
+        <Button className="button"
+          onClick={cancelOrder}
+          color="#047E61"
+          disabled={() => modifyTime - time >= 0}
+        >Cancel Order</Button>
       </div>  
     </div>
   );
