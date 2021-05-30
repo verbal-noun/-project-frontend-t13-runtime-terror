b474dade9640f7a1fef4a445a19125de066bcdfa
Kamyar <kkarimifakhr@student.unimelb.edu.au>
2021-05-30 22:55:54 +1000

    final css styling done

--- customer/src/userPage.js
+++ customer/src/userPage.js
@@ -33,7 +33,7 @@ function OrderCard(props) {
           <span className="order-header">{props.order.vendorName} is creating your order!</span>
           <div className="order-status">{props.order.status}</div>
         </div><br/>
-        <Button className="button" onClick={props.setSelected}>View Details</Button>
+        <Button className="button" id="detail" onClick={props.setSelected}>View Details</Button>
 
         <div className="order-time">{elapsed(props.order.createdWhen)+" "}</div>
     </div>
